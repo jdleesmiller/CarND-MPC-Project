ebe47d94905feade07caf8e81d4e5a30ce11f044
John Lees-Miller <jdleesmiller@gmail.com>
2017-05-28 21:00:53 +0100

    Tidy up problem builder

--- src/problem.cpp
+++ src/problem.cpp
@@ -54,30 +54,66 @@ Problem::Problem(const ReferencePolynomial &reference) :
 // `vars` is a vector containing the variable values (state & actuators).
 void Problem::operator()(ADvector& fg, const ADvector& vars) {
   // The cost is stored is the first element of `fg`.
-  // Any additions to the cost should be added to `fg[0]`.
+  // We add 1 to each of the starting indices due to cost being located at
+  // index 0 of `fg`. This bumps up the position of all the other values.
   fg[0] = 0;
 
-  // The part of the cost based on the reference state.
-  for (int i = 0; i < N; i++) {
-    AD<double> x0 = vars[x_start + i];
-    AD<double> v0 = vars[v_start + i];
-    AD<double> delta0 = vars[delta_start + i];
+  //
+  // Initial value constraints
+  //
+  fg[1 + x_start] = vars[x_start];
+  fg[1 + y_start] = vars[y_start];
+  fg[1 + psi_start] = vars[psi_start];
+  fg[1 + v_start] = vars[v_start];
 
-    // The reference angle comes from the derivative of the reference
-    // polynomial, which here is written with the Horner scheme.
-    AD<double> reference_slope =
+  for (int i = 0; i < N - 1; i++) {
+    // The state at time t.
+    const AD<double> &x0 = vars[x_start + i];
+    const AD<double> &y0 = vars[y_start + i];
+    const AD<double> &psi0 = vars[psi_start + i];
+    const AD<double> &v0 = vars[v_start + i];
+
+    // The controls at time t.
+    const AD<double> &delta0 = vars[delta_start + i];
+    const AD<double> &throttle0 = vars[throttle_start + i];
+    const AD<double> &a0 = throttle_to_acceleration(throttle0, v0);
+
+    // The state at time t+1.
+    const AD<double> &x1 = vars[x_start + i + 1];
+    const AD<double> &y1 = vars[y_start + i + 1];
+    const AD<double> &psi1 = vars[psi_start + i + 1];
+    const AD<double> &v1 = vars[v_start + i + 1];
+
+    //
+    // State Constraints
+    // Each of these expressions is constrained to be zero.
+    //
+
+    fg[2 + x_start + i] = x1 - (x0 + v0 * CppAD::cos(psi0) * dt);
+    fg[2 + y_start + i] = y1 - (y0 + v0 * CppAD::sin(psi0) * dt);
+    fg[2 + psi_start + i] = psi1 - (psi0 + v0 * delta0 / Lf * dt);
+    fg[2 + v_start + i] = v1 - (v0 + a0 * dt);
+
+    //
+    // Objective
+    //
+
+    // Steering angle error: The reference angle comes from the derivative of
+    // the reference polynomial, which here is written with the Horner scheme.
+    const AD<double> &reference_slope =
       reference.coeffs[1] + x0 * (
         2 * reference.coeffs[2] + x0 * (
           3 * reference.coeffs[3]
         )
       );
-    AD<double> epsi_i = (vars[psi_start + i] - CppAD::atan(reference_slope)) +
-      v0 * delta0 / Lf * dt;
-    // std::cout << "epsi" << i << " = " << epsi_i << std::endl;
-    fg[0] += epsi_weight * CppAD::pow(epsi_i, 2);
+    const AD<double> &psides0 = CppAD::atan(reference_slope);
+    const AD<double> &epsi0 = (psi0 - psides0) + v0 * delta0 / Lf * dt;
+    fg[0] += epsi_weight * CppAD::pow(epsi0, 2);
 
-    // Very approximately...
-    AD<double> reference_y =
+    // Cross track error: We just use the y coordinate of the reference
+    // polynomial to find the CTE. This is approximately right when both the
+    // car's steering angle (psi) and the reference slope are not too steep.
+    const AD<double> &reference_y =
       reference.coeffs[0] + x0 * (
         reference.coeffs[1] + x0 * (
           reference.coeffs[2] + x0 * (
@@ -85,75 +121,24 @@ void Problem::operator()(ADvector& fg, const ADvector& vars) {
           )
         )
       );
-    AD<double> cte_i = (reference_y - vars[y_start + i]) +
-       v0 * CppAD::sin(epsi_i) * dt;
-    // std::cout << "cte" << i << " = " << cte_i << std::endl;
-    fg[0] += cte_weight * CppAD::pow(cte_i, 2);
+    const AD<double> &cte0 = (reference_y - y0) + v0 * CppAD::sin(epsi0) * dt;
+    fg[0] += cte_weight * CppAD::pow(cte0, 2);
 
+    // Speed: Just have to be careful of the units.
     fg[0] += v_weight * CppAD::pow(vars[v_start + i] -
       ref_v * MPH_TO_METERS_PER_SECOND, 2);
-  }
 
-  // Minimize the use of actuators.
-  for (int i = 0; i < N - 1; i++) {
-    fg[0] += delta_weight * CppAD::pow(vars[delta_start + i], 2);
-    fg[0] += throttle_weight * CppAD::pow(vars[throttle_start + i], 2);
-  }
+    // Actuators: Minimize the use of actuators.
+    fg[0] += delta_weight * CppAD::pow(delta0, 2);
+    fg[0] += throttle_weight * CppAD::pow(throttle0, 2);
 
-  // Minimize the value gap between sequential actuations.
-  for (int i = 0; i < N - 2; i++) {
-    fg[0] += delta_gap_weight *
-      CppAD::pow(vars[delta_start + i + 1] - vars[delta_start + i], 2);
-    fg[0] += throttle_gap_weight *
-      CppAD::pow(vars[throttle_start + i + 1] - vars[throttle_start + i], 2);
-  }
-
-  //
-  // Setup Constraints
-  //
-  // NOTE: In this section you'll setup the model constraints.
-
-  // Initial constraints
-  //
-  // We add 1 to each of the starting indices due to cost being located at
-  // index 0 of `fg`.
-  // This bumps up the position of all the other values.
-  fg[1 + x_start] = vars[x_start];
-  fg[1 + y_start] = vars[y_start];
-  fg[1 + psi_start] = vars[psi_start];
-  fg[1 + v_start] = vars[v_start];
-
-  // The rest of the constraints
-  for (int i = 0; i < N - 1; i++) {
-    // The state at time t+1 .
-    AD<double> x1 = vars[x_start + i + 1];
-    AD<double> y1 = vars[y_start + i + 1];
-    AD<double> psi1 = vars[psi_start + i + 1];
-    AD<double> v1 = vars[v_start + i + 1];
-
-    // The state at time t.
-    AD<double> x0 = vars[x_start + i];
-    AD<double> y0 = vars[y_start + i];
-    AD<double> psi0 = vars[psi_start + i];
-    AD<double> v0 = vars[v_start + i];
-
-    // Only consider the actuation at time t.
-    AD<double> delta0 = vars[delta_start + i];
-    AD<double> a0 = throttle_to_acceleration(vars[throttle_start + i], v0);
-
-    // Here's `x` to get you started.
-    // The idea here is to constraint this value to be 0.
-    //
-    // Recall the equations for the model:
-    // x_[t+1] = x[t] + v[t] * cos(psi[t]) * dt
-    // y_[t+1] = y[t] + v[t] * sin(psi[t]) * dt
-    // psi_[t+1] = psi[t] + v[t] / Lf * delta[t] * dt
-    // v_[t+1] = v[t] + a[t] * dt
-    // cte[t+1] = f(x[t]) - y[t] + v[t] * sin(epsi[t]) * dt
-    // epsi[t+1] = psi[t] - psides[t] + v[t] * delta[t] / Lf * dt
-    fg[2 + x_start + i] = x1 - (x0 + v0 * CppAD::cos(psi0) * dt);
-    fg[2 + y_start + i] = y1 - (y0 + v0 * CppAD::sin(psi0) * dt);
-    fg[2 + psi_start + i] = psi1 - (psi0 + v0 * delta0 / Lf * dt);
-    fg[2 + v_start + i] = v1 - (v0 + a0 * dt);
+    // Actuator smoothness: Minimize the value gap between sequential
+    // actuations.
+    if (i < N - 2) {
+      fg[0] += delta_gap_weight *
+        CppAD::pow(vars[delta_start + i + 1] - delta0, 2);
+      fg[0] += throttle_gap_weight *
+        CppAD::pow(vars[throttle_start + i + 1] - throttle0, 2);
+    }
   }
 }
